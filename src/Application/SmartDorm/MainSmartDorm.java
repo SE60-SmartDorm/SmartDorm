04b6bd9d25988b6e703f6ffb9c69caa00b211b8c
MrBasZ <58010819@kmitl.ac.th>
2017-10-29 19:42:24 +0700

    initial Main source file

---
+++ src/Application/SmartDorm/MainSmartDorm.java
@@ -0,0 +1,30 @@
+package Application.SmartDorm;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainSmartDorm extends Application {
+
+    Stage stage;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        this.stage = primaryStage;
+
+        Parent root = FXMLLoader.load(getClass().getResource("UI/Manage/TenantManage.fxml"));
+
+        Scene scene = new Scene(root);
+
+        stage.setTitle("Smart Dorm");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
