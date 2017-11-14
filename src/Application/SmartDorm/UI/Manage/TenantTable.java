1444715d53c3b35d1cfa60b8b1e88cea7b0b9514
MrBasZ <58010819@kmitl.ac.th>
2017-11-15 03:30:39 +0700

    [MISC] Change class name

--- src/Application/SmartDorm/UI/Manage/TenantTable.java
+++ src/Application/SmartDorm/UI/Manage/TenantTable.java
@@ -10,10 +10,10 @@ import javafx.beans.property.StringProperty;
  * @author EIEI ZA
  */
 
-public class PeopleExData extends RecursiveTreeObject<PeopleExData> {
-    StringProperty room, status, name, startDate, endDate;
+public class TenantTable extends RecursiveTreeObject<TenantTable> {
+    private StringProperty room, status, name, startDate, endDate;
 
-    public PeopleExData(String room, String status, String name, String sartDate, String endDate) {
+    public TenantTable(String room, String status, String name, String sartDate, String endDate) {
         this.room = new SimpleStringProperty(room);
         this.status = new SimpleStringProperty(status);
         this.name = new SimpleStringProperty(name);
@@ -25,59 +25,59 @@ public class PeopleExData extends RecursiveTreeObject<PeopleExData> {
         return room.get();
     }
 
-    public StringProperty roomProperty() {
-        return room;
-    }
-
     public void setRoom(String room) {
         this.room.set(room);
+    }
+
+    public StringProperty roomProperty() {
+        return room;
     }
 
     public String getStatus() {
         return status.get();
     }
 
-    public StringProperty statusProperty() {
-        return status;
-    }
-
     public void setStatus(String status) {
         this.status.set(status);
+    }
+
+    public StringProperty statusProperty() {
+        return status;
     }
 
     public String getName() {
         return name.get();
     }
 
-    public StringProperty nameProperty() {
-        return name;
-    }
-
     public void setName(String name) {
         this.name.set(name);
+    }
+
+    public StringProperty nameProperty() {
+        return name;
     }
 
     public String getStartDate() {
         return startDate.get();
     }
 
-    public StringProperty startDateProperty() {
-        return startDate;
-    }
-
     public void setStartDate(String startDate) {
         this.startDate.set(startDate);
+    }
+
+    public StringProperty startDateProperty() {
+        return startDate;
     }
 
     public String getEndDate() {
         return endDate.get();
     }
 
-    public StringProperty endDateProperty() {
-        return endDate;
-    }
-
     public void setEndDate(String endDate) {
         this.endDate.set(endDate);
+    }
+
+    public StringProperty endDateProperty() {
+        return endDate;
     }
 }
