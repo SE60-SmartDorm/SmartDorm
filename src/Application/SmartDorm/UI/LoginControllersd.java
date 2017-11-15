4c76da5317335c7f433e9f08a5a641aa3ca06338
ToonRaseebut <anuchagoku@gmail.com>
2017-11-15 20:13:09 +0700

    [EDIT] Edit Login Controller select type of user

--- src/Application/SmartDorm/UI/LoginControllersd.java
+++ src/Application/SmartDorm/UI/LoginControllersd.java
@@ -4,16 +4,20 @@ package Application.SmartDorm.UI;
 import Application.SmartDorm.MainSmartDorm;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.event.ActionEvent;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.awt.*;
 import java.io.IOException;
+import java.util.Observable;
 
 
 public class LoginControllersd {
@@ -23,24 +27,55 @@ public class LoginControllersd {
     JFXPasswordField textPassword;
     @FXML
     Label warning;
+    @FXML
+    Label warning2;
 
-    String user = "anucha";
-    String pass = "123456789";
+    @FXML
+    public ComboBox<String> status;
+    ObservableList<String> list = FXCollections.observableArrayList("Owner","Tenant");
+
+    String ownerUser = "anucha";
+    String ownerPass = "123456789";
+    String tenantUser = "pongdanai";
+    String tenantPass = "987654321";
+    String type;
+
+    Stage tenant_stage = MainSmartDorm.getStage();
+
+    public void initialize(){
+        status.setItems(list);
+    }
 
     @FXML
     private void loginPress(ActionEvent event) throws IOException {
-        if((user.equals(textID.getText())) && (pass.equals(textPassword.getText())))
+        type = status.getValue();
+        warning.setVisible(false);
+        warning2.setVisible(false);
+
+        if("Owner".equals(type)) {
+            if ((ownerUser.equals(textID.getText())) && (ownerPass.equals(textPassword.getText()))) {
+                Parent home_tenant_payment = FXMLLoader.load(getClass().getResource("OwnerDashboard.fxml"));
+                Scene tenant3rd_page = new Scene(home_tenant_payment);
+
+                tenant_stage.setScene(tenant3rd_page);
+                tenant_stage.show();
+            } else
+                warning.setVisible(true);
+        }
+
+        else if("Tenant".equals(type))
         {
-            Stage tenant_stage = MainSmartDorm.getStage();
+            if ((tenantUser.equals(textID.getText())) && (tenantPass.equals(textPassword.getText()))) {
+                Parent home_tenant_payment = FXMLLoader.load(getClass().getResource("TenantDashboard.fxml"));
+                Scene tenant3rd_page = new Scene(home_tenant_payment);
 
-            Parent home_tenant_payment = FXMLLoader.load(getClass().getResource("OwnerDashboard.fxml"));
-            Scene tenant3rd_page = new Scene(home_tenant_payment);
-
-            tenant_stage.setScene(tenant3rd_page);
-            tenant_stage.show();
+                tenant_stage.setScene(tenant3rd_page);
+                tenant_stage.show();
+            } else
+                warning.setVisible(true);
         }
         else
-            warning.setVisible(true);
+            warning2.setVisible(true);
     }
 
 }
