4d4c9858b8de384e3e1c99cfbe7efb5f5ba71619
MrBasZ <58010819@kmitl.ac.th>
2017-11-17 20:17:13 +0700

    [DEV] Add getter and setter variable

--- src/Application/SmartDorm/UI/Manage/TenantStudentInfoController.java
+++ src/Application/SmartDorm/UI/Manage/TenantStudentInfoController.java
@@ -1,18 +1,54 @@
 package Application.SmartDorm.UI.Manage;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class TenantStudentInfoController implements Initializable {
+public class TenantStudentInfoController{
+
+    @FXML
+    private TextField studentEduTF;
+
+    @FXML
+    private TextField studentFacultyTF;
+
+    @FXML
+    private TextField studentGradeTF;
+
+    //Getter and Setter
+    public TextField getStudentEduTF() {
+        return studentEduTF;
+    }
+
+    public void setStudentEduTF(TextField studentEduTF) {
+        this.studentEduTF = studentEduTF;
+    }
+
+    public TextField getStudentFacultyTF() {
+        return studentFacultyTF;
+    }
+
+    public void setStudentFacultyTF(TextField studentFacultyTF) {
+        this.studentFacultyTF = studentFacultyTF;
+    }
+
+    public TextField getStudentGradeTF() {
+        return studentGradeTF;
+    }
+
+    public void setStudentGradeTF(TextField studentGradeTF) {
+        this.studentGradeTF = studentGradeTF;
+    }
 
     /**
      * Initializes the controller class. This method is automatically called
      * after the fxml file has been loaded.
      */
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
+    public void initialize() {
 
     }
 }
--- src/Application/SmartDorm/UI/Manage/TenantTeacherInfoController.java
+++ src/Application/SmartDorm/UI/Manage/TenantTeacherInfoController.java
@@ -1,18 +1,49 @@
 package Application.SmartDorm.UI.Manage;
 
-import javafx.fxml.Initializable;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
-import java.net.URL;
-import java.util.ResourceBundle;
+public class TenantTeacherInfoController {
 
-public class TenantTeacherInfoController implements Initializable{
+    @FXML
+    private TextField teacherEduTF;
+
+    @FXML
+    private TextField teacherFacultyTF;
+
+    @FXML
+    private TextField teacherPositionTF;
+
+    //Getter and Setter
+    public TextField getTeacherEduTF() {
+        return teacherEduTF;
+    }
+
+    public void setTeacherEduTF(TextField teacherEduTF) {
+        this.teacherEduTF = teacherEduTF;
+    }
+
+    public TextField getTeacherFacultyTF() {
+        return teacherFacultyTF;
+    }
+
+    public void setTeacherFacultyTF(TextField teacherFacultyTF) {
+        this.teacherFacultyTF = teacherFacultyTF;
+    }
+
+    public TextField getTeacherPositionTF() {
+        return teacherPositionTF;
+    }
+
+    public void setTeacherPositionTF(TextField teacherPositionTF) {
+        this.teacherPositionTF = teacherPositionTF;
+    }
 
     /**
      * Initializes the controller class. This method is automatically called
      * after the fxml file has been loaded.
      */
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
+    public void initialize() {
 
     }
 }
