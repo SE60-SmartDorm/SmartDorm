6ef6271af0c655b0f6af7ee2d785398815bf0aea
MrBasZ <58010819@kmitl.ac.th>
2017-11-17 12:57:54 +0700

    [ADD] LeasesForm, TenantStudentForm, TenantTeacherForm Controller

---
+++ src/Application/SmartDorm/UI/Manage/LeasesFormController.java
@@ -0,0 +1,18 @@
+package Application.SmartDorm.UI.Manage;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LeasesFormController implements Initializable {
+
+    /**
+     * Initializes the controller class. This method is automatically called
+     * after the fxml file has been loaded.
+     */
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
