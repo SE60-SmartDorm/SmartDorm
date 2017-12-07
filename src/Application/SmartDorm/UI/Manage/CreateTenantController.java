257fcc09c0948c8bfc84e8071775a0b93bd39b3d
MrBasZ <58010819@kmitl.ac.th>
2017-12-07 15:30:47 +0700

    [DEV] Add alert function

--- src/Application/SmartDorm/UI/Manage/CreateTenantController.java
+++ src/Application/SmartDorm/UI/Manage/CreateTenantController.java
@@ -2,18 +2,24 @@ package Application.SmartDorm.UI.Manage;
 
 import Application.SmartDorm.UI.OwnerMainController;
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXDialog;
+import com.jfoenix.controls.JFXDialogLayout;
 import de.jensd.fx.glyphs.materialdesignicons.MaterialDesignIconView;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.control.TreeItem;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
+import java.io.IOException;
 import java.time.format.DateTimeFormatter;
 
 public class CreateTenantController {
-
     //Set TenantFormController
     @FXML
     public Parent embedTenantForm;
@@ -23,7 +29,8 @@ public class CreateTenantController {
     FXMLLoader loader = new FXMLLoader();
     @FXML
     JFXButton saveDataBT;
-    String roomID, status, name, begin, end, note;
+    //other
+    String roomID, status, name, begin, end, note, phone, address, IDCard;
     TreeItem<TenantTable> treeItemTenant = OwnerMainController.manageController.getTenantTableView().getSelectionModel().getSelectedItem();
     @FXML
     private TenantFormController embedTenantFormController;
@@ -33,6 +40,8 @@ public class CreateTenantController {
     private LeasesFormController embedLeasesFormController;
     @FXML
     private MaterialDesignIconView iconClose;
+    @FXML
+    private StackPane alertError;
 
     public void initialize() {
         autoFillDataLeases();
@@ -41,14 +50,20 @@ public class CreateTenantController {
     @FXML
     void saveData(ActionEvent event) {
         //TODO Save tenant data to DATABASE
-        setTenantData();
-        //OwnerMainController.manageController.getTenantTableData().add(new TenantTable(roomID,status,name,begin,end));
-        TenantTable n = new TenantTable(roomID, status, name, begin, end, note);
-        treeItemTenant.setValue(n);
-        OwnerMainController.manageController.getTenantTableView().getSelectionModel().clearSelection();
-        OwnerMainController.manageController.tenantSetButtonDisable();
-        saveDataBT.getScene().getWindow().hide();
-        System.out.println("Save data");
+        if (embedLeasesFormController.checkBeforeSubmitLeases() && embedLeasesFormController.checkBeforeSubmitLeases()) {
+            setTenantData();
+
+            TenantTable n = new TenantTable(roomID, status, name, begin, end, note);
+            treeItemTenant.setValue(n);
+
+            OwnerMainController.manageController.getTenantTableView().getSelectionModel().clearSelection();
+            OwnerMainController.manageController.tenantSetButtonDisable();
+            saveDataBT.getScene().getWindow().hide();
+            System.out.println("Save data");
+        } else {
+            alertFail();
+            System.out.println("Error");
+        }
     }
 
     //Close Windows state
@@ -80,11 +95,53 @@ public class CreateTenantController {
         OwnerMainController.manageController.tenantSetButtonDisable();
     }
 
+    private void setAutoFillData() {
+        if (!(embedTenantFormController.getPrefixNameCB1().getSelectionModel().isEmpty() || embedTenantFormController.getPrefixNameCB1().getSelectionModel().getSelectedItem() == null)) {
+            name = embedTenantFormController.getPrefixNameCB1().getSelectionModel().getSelectedItem() + " " +
+                    embedTenantFormController.getFirstNameTF1().getText() + " " +
+                    embedTenantFormController.getLastNameTF1().getText();
+        }
+
+        phone = embedTenantFormController.getPhoneNumberTF1().getText();
+        address = embedTenantFormController.getAddressTF1().getText();
+        IDCard = embedTenantFormController.getIdCardTF1().getText();
+    }
+
     private void autoFillDataLeases() {
         embedLeasesFormController.getAutoFillData().setOnAction(event -> {
-            setTenantData();
+            setAutoFillData();
+            embedLeasesFormController.getNameLeasesTF().requestFocus();
             embedLeasesFormController.getNameLeasesTF().setText(name);
-            embedLeasesFormController.getPhoneLeasesTF().setText("xxx");
+            embedLeasesFormController.getPhoneLeasesTF().requestFocus();
+            embedLeasesFormController.getPhoneLeasesTF().setText(phone);
+            embedLeasesFormController.getAddressLeasesTF().setText(address);
+            embedLeasesFormController.getIdCardLeasesTF().requestFocus();
+            embedLeasesFormController.getIdCardLeasesTF().setText(IDCard);
+            saveDataBT.requestFocus();
         });
+    }
+
+    private void alertFail() {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../Alert/Fail.fxml"));
+        StackPane icon = null;
+        try {
+            icon = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Text text = new Text("กรุณากรอกข้อมูลให้ถูกต้อง");
+        VBox header = new VBox();
+        VBox detail = new VBox();
+        header.getChildren().add(icon);
+        header.setAlignment(Pos.CENTER);
+        detail.setStyle("-fx-font-family: 'Sukhumvit Set'; -fx-font-size: 22;");
+        detail.getChildren().add(text);
+        detail.setAlignment(Pos.CENTER);
+
+        JFXDialogLayout content = new JFXDialogLayout();
+        content.setHeading(header);
+        content.setBody(detail);
+        JFXDialog dialog = new JFXDialog(alertError, content, JFXDialog.DialogTransition.CENTER);
+        dialog.show();
     }
 }
